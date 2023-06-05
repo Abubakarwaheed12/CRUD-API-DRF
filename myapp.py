d4ac959c1e43145e457e0d462f0665bbd2fc16d8
abubakarwaheed12 <abubakarjutt6346527@gmail.com>
2023-06-05 18:11:55 +0500

    ..

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ import requests
 
 
 
-URL="http://127.0.0.1:8000/" 
+URL="http://127.0.0.1:8000" 
 
 
 # Get Data
@@ -23,48 +23,48 @@ def get_data( id = None):
     print(res_data)
 
 
-# get_data()   
+get_data()   
 
 
 
 # Insert Data 
-def post_data():
-    data={
-        'name':'mmmmmmmm',
-        'status':'PENDING',
-    }
+# def post_data():
+#     data={
+#         'name':'mmmmmmmm',
+#         'status':'PENDING',
+#     }
     
         
-    json_data=json.dumps(data)
+#     json_data=json.dumps(data)
     
-    headers={'content-type':'application/json'}
-    r=requests.post(url = URL , headers=headers ,  data = json_data)
+#     headers={'content-type':'application/json'}
+#     r=requests.post(url = URL , headers=headers ,  data = json_data)
      
-    res_data=r.json()
+#     res_data=r.json()
     
-    print(res_data)
+#     print(res_data)
 
 # post_data()
 
 
 # Update Data 
 
-def update_data():
-    data={
-        'id':13,
-        'name':' bakar',
-        'status':'DONE',
-    }
+# def update_data():
+#     data={
+#         'id':13,
+#         'name':' bakar',
+#         'status':'DONE',
+#     }
     
         
-    json_data=json.dumps(data)
+#     json_data=json.dumps(data)
     
-    headers={'content-type':'application/json'}
-    r=requests.put(url = URL , headers=headers ,  data = json_data)
+#     headers={'content-type':'application/json'}
+#     r=requests.put(url = URL , headers=headers ,  data = json_data)
      
-    res_data=r.json()
+#     res_data=r.json()
     
-    print(res_data)
+#     print(res_data)
 
 # update_data()
 
@@ -72,15 +72,15 @@ def update_data():
 
 
 # Delete Data 
-def delete_data(id):
-    data={'id':id,}
-    json_data=json.dumps(data)
+# def delete_data(id):
+#     data={'id':id,}
+#     json_data=json.dumps(data)
     
-    headers={'content-type':'application/json'}
-    r=requests.delete(url = URL , headers=headers ,  data = json_data)
+#     headers={'content-type':'application/json'}
+#     r=requests.delete(url = URL , headers=headers ,  data = json_data)
     
-    res_data=r.json()
+#     res_data=r.json()
     
-    print(res_data)
+#     print(res_data)
 
-delete_data(3)
+# delete_data(3)
